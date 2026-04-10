2c064c5a8c1f576c81fa8da8747c40e41bbdb6ee
Zeynep Caysar <zeynepcaysar@gmail.com>
2026-04-10 13:43:25 +0200

    merged with tfidf and kg implementation updates

--- src/config.py
+++ src/config.py
@@ -12,13 +12,7 @@ HPO_PATH = ONTOLOGY_DIR / "hpo.owl"
 ORDO_PATH = ONTOLOGY_DIR / "ordo.owl"
 MONDO_PATH = ONTOLOGY_DIR / "mondo_rare.owl"
 HOOM_PATH = ONTOLOGY_DIR / "hoom.owl"
-HPOA_PATH = ONTOLOGY_DIR / "phenotype.hpoa"
-
-ORPHADATA_PRODUCT4_PATH = ONTOLOGY_DIR / "en_product4_HPO.xml"
-
-MONARCH_DISEASE_TO_HPO_PATH = (
-    ONTOLOGY_DIR / "disease_to_phenotypic_feature_association.all.tsv.gz"
-)
+HPOA_PATH = ONTOLOGY_DIR / "phenotype.hpoa.owl"
 
 ONTOLOGY_PATHS = {
     "hpo": HPO_PATH,
@@ -26,8 +20,6 @@ ONTOLOGY_PATHS = {
     "mondo": MONDO_PATH,
     "hoom": HOOM_PATH,
     "hpoa": HPOA_PATH,
-    "orphadata_product4": ORPHADATA_PRODUCT4_PATH,
-    "monarch_disease_hpo": MONARCH_DISEASE_TO_HPO_PATH,
 }
 
 CANONICAL_DISEASE_NAMESPACE = "ORPHA"
@@ -36,9 +28,11 @@ MIN_DISEASE_HPO_TERMS = 1
 
 EXAMPLE_PATIENT = {
     "patient_id": "patient_001",
-    "raw_text": (
-        "Patient with developmental delay, cerebellar ataxia, "
-        "and anemia."
-    ),
+    "raw_text": ("Patient with developmental delay, cerebellar ataxia, " "and anemia."),
     "hpo_terms": ["HP:0001263", "HP:0002470", "HP:0001903"],
 }
+
+NEO4J_URI = "bolt://localhost:7687"
+NEO4J_USER = "neo4j"
+NEO4J_PASSWORD = "raresim123"
+NEO4J_DATABASE = "neo4j"
